fcf7f510190165267dca5c635b6fb5adac0ff7ef
Kirill Potapchuk <qeliathus@gmail.com>
2024-07-31 22:05:29 +0300

    Revert "init"
    This reverts commit 8acaf9b122de59ce1231e6b03408220b44b35b71.

--- src/main/java/by/potapchuk/SearchProducts/repository/SkuRepository.java
+++
@@ -1,9 +0,0 @@
-package by.potapchuk.SearchProducts.repository;
-
-import by.potapchuk.SearchProducts.core.entity.Sku;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkuRepository extends JpaRepository<Sku, Long> {
-}
